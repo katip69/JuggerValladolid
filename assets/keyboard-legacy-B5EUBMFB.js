7668fd7174bbfdacf70504b64f6cf9d098d6e6c7
katip69 <miguelrasero1999@hotmail.es>
2024-06-26 23:39:41 +0200

    Updates

--- assets/keyboard-legacy-B5EUBMFB.js
+++
@@ -1,8 +0,0 @@
-System.register(["./index-legacy-DNhgdnBT.js"],(function(e,n){"use strict";var i;return{setters:[function(e){i=e.w}],execute:function(){
-/*!
-             * (C) Ionic http://ionicframework.com - MIT License
-             */
-var n,t;
-/*!
-             * (C) Ionic http://ionicframework.com - MIT License
-             */!function(e){e.Unimplemented="UNIMPLEMENTED",e.Unavailable="UNAVAILABLE"}(n||(n={})),e("a",t),function(e){e.Body="body",e.Ionic="ionic",e.Native="native",e.None="none"}(t||e("a",t={})),e("K",{getEngine:function(){var e=function(){if(void 0!==i)return i.Capacitor}();if(null==e?void 0:e.isPluginAvailable("Keyboard"))return e.Plugins.Keyboard},getResizeMode:function(){var e=this.getEngine();return(null==e?void 0:e.getResizeMode)?e.getResizeMode().catch((function(e){if(e.code!==n.Unimplemented)throw e})):Promise.resolve(void 0)}})}}}));
