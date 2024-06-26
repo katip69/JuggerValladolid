4bc395bb21841ce20ef5dda7fbd842a14dfdf030
katip69 <miguelrasero1999@hotmail.es>
2024-06-26 23:32:18 +0200

    Configuración final

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <ion-page>
     <ion-header style="height: 5rem; max-height: 5rem; ">
       <ion-row class="ion-justify-content-center ion-align-items-center">
-        <ion-img src="src/Imagenes/images.jpeg" style="height: 5rem;"></ion-img>
+        <ion-img src="./Imagenes/images.jpeg" style="height: 5rem;"></ion-img>
         <ion-row fixed="true" style="margin-left: 3rem;">
           <ion-label @click="$router.push({name:'Home'})">Inicio </ion-label> 
           <ion-label @click="$router.push({name:'QuienesSomos'})"> Quienes Somos</ion-label>
@@ -16,6 +16,11 @@
     <ion-content :fullscreen="true" class="ion-padding">
       <ion-router-outlet slot="fixed"/>
     </ion-content>
+    <ion-footer >
+      <ion-row style="margin-top: 20px;" class="ion-justify-content-center">
+        <ion-label style="color: white;"> Jugger Pucela</ion-label>
+      </ion-row>
+    </ion-footer>
     </ion-page>
   </ion-app>
 </template>
@@ -30,7 +35,11 @@ import { IonApp,IonImg,IonRow,IonLabel, IonHeader, IonRouterOutlet,IonContent,Io
     margin-right: 20px;
     cursor: pointer;
   }
-
+  ion-footer{
+    background-color: purple;
+    height: 7rem;
+    box-shadow: 0px -5px 10px rgba(0, 0, 0, 0.1);
+  }
 
 </style>
 
